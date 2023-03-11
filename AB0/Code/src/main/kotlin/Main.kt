4ab6747155369c621574866e69f36f1cbc40c821
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-03-11 16:01:02 +0100

    refactor: modified dummy values

--- AB0/Code/src/main/kotlin/Main.kt
+++ AB0/Code/src/main/kotlin/Main.kt
@@ -2,13 +2,23 @@ fun main(args: Array<String>) {
     println("Hello World!")
     println("Program arguments: ${args.joinToString()}")
 
-    var vehicle = Vehicle(1, "test",
-        Brand("B1", "BC1", "43 9381283", "asdasd"),
-        Workshop("W1", "WC1", 9020, "W2", "street", "43 1231237"),
-        10, 100, 10.0, 100.0)
+    val workshops = ArrayList<Workshop>()
+    workshops.add(Workshop("Workshop Nr 1", "Autria", 9020, "Klagenfurt", "Street1", "+43 664 3827371"))
+    workshops.add(Workshop("Workshop Nr 2", "Autria", 9030, "Villach", "Street2", "+43 664 3827371"))
+    workshops.add(Workshop("Workshop Nr 3", "Autria", 9040, "Wolfsberg", "Street3", "+43 664 3827371"))
+    workshops.add(Workshop("Workshop Nr 4", "Autria", 9050, "St. Veit", "Street4", "+43 664 3827371"))
+    workshops.add(Workshop("Workshop Nr 5", "Autria", 9060, "Feldkirchen", "Street5", "+43 664 3827371"))
 
-    vehicle.printInfo()
+    val vehicles = ArrayList<Vehicle>()
+
+    val dummyBrand = Brand("CarBrand", "Austria", "+43 182 1238138", "em@mail.at")
 
 
+    vehicles.add(Vehicle(1, "Audi A1", dummyBrand, workshops, 100, 12, generateValues(60, 5).toDouble(), 100.0))
+    vehicles.add(Vehicle(2, "Audi A2", dummyBrand, workshops, 100, 12, generateValues(60, 5).toDouble(), 100.0))
+    vehicles.add(Vehicle(3, "Audi A3", dummyBrand, workshops, 100, 12, generateValues(60, 5).toDouble(), 100.0))
+    vehicles.add(Vehicle(4, "Audi A4", dummyBrand, workshops, 100, 12, generateValues(60, 5).toDouble(), 100.0))
+    vehicles.add(Vehicle(5, "Audi A5", dummyBrand, workshops, 100, 12, generateValues(60, 5).toDouble(), 100.0))
 
+    for(vehicle in vehicles) vehicle.printInfo()
 }
