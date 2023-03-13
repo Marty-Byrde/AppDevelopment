eb659c3ab7ee03ac903224b765e631f534a8c87b
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-03-13 14:25:05 +0100

    feat: added nextPlayer functionality

--- Project/Code/app/src/main/java/com/example/madn/classes/Map.kt
+++ Project/Code/app/src/main/java/com/example/madn/classes/Map.kt
@@ -3,6 +3,9 @@ package com.example.madn.classes
 class Map (val players: ArrayList<Player>) {
     val figures : ArrayList<Figure> = ArrayList()
 
+    // the player who is selected to make his move
+    private var movingPlayer: Player = players[0]
+
 
 
 
@@ -36,6 +39,21 @@ class Map (val players: ArrayList<Player>) {
         kicked?.resetPosition()
 
         figure.move(destination)
+    }
+
+    /**
+     * Sets a new player whose turn it is to make a move. When a [player] is provided, then this player can make his move. (e.g. when the dice threw 6)
+     */
+    fun nextPlayer(player: Player? = null){
+        if(player != null) movingPlayer = player;
+
+        val indexOfPlayer = this.players.indexOf(this.movingPlayer)
+
+        if(indexOfPlayer + 1 == this.players.size)
+            this.movingPlayer = this.players[0]
+        else
+            this.movingPlayer = this.players[indexOfPlayer + 1]
+
     }
 
 }
