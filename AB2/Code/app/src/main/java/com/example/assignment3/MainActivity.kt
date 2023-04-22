64c6d6ec0cc5be5e77a6682952c0f694f2d8fb3c
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-04-22 14:11:50 +0200

    feat: added clickListeners for task buttons
    Dynamically opens the activity of a task, depending on the button pressed (Task 1, Task 2, Task 3, Task 4).

--- AB2/Code/app/src/main/java/com/example/assignment3/MainActivity.kt
+++ AB2/Code/app/src/main/java/com/example/assignment3/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.assignment3
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.assignment3.TouchControl.SurfaceActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clickListener = View.OnClickListener { element ->
+            val button = element as Button;
+            val text = button.text.toString()
+            Log.d("APPDEV", "${text} has been pressed!")
+
+            //* Cancel clickhandler
+            if(!text.toLowerCase().startsWith("task")) return@OnClickListener;
+
+            val task = text.split(" ")[1].toString().toInt();
+
+            var intent = Intent(applicationContext, MainActivity::class.java);
+            when(task){
+                4 -> intent = Intent(applicationContext, SurfaceActivity::class.java)
+            }
+
+            startActivity(intent)
+        }
+
+
+        val task1 = findViewById<Button>(R.id.task1)
+        val task2 = findViewById<Button>(R.id.task2)
+        val task3 = findViewById<Button>(R.id.task3)
+        val task4 = findViewById<Button>(R.id.task4)
+
+        task1.setOnClickListener(clickListener)
+        task2.setOnClickListener(clickListener)
+        task3.setOnClickListener(clickListener)
+        task4.setOnClickListener(clickListener)
+
+
+        task4.performClick()
     }
+
+
 }
