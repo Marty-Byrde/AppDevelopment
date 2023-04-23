ae6f58dd725b1764f25264c875d18a00260fbe1c
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-04-23 15:01:48 +0200

    feat: added fragments

---
+++ AB2/Code/app/src/main/java/com/example/assignment3/Fragments/FirstFragment.kt
@@ -0,0 +1,34 @@
+package com.example.assignment3.Fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.*
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.assignment3.R
+
+class FirstFragment : Fragment(R.layout.first_fragment) {
+    var parent : FragmentActivity? = null;
+    private lateinit var _view : View;
+
+    fun createLink(activity:FragmentActivity){
+        parent = activity;
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        this._view = view;
+        var count = 0;
+
+        val button = view.findViewById<Button>(R.id.btnRespondF1)
+        button.setOnClickListener{ parent?.receiveMessage(" ${javaClass.simpleName} has responded ${count++} times!")}
+    }
+
+
+    fun receiveMessage(message: String){
+        Log.d("AppDev", "Fragment has received a message! $message")
+        this._view.findViewById<TextView>(R.id.txtCommunicateF2).text = message
+    }
+}
