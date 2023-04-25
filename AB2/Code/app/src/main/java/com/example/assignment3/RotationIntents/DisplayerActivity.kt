c32ae2921b90c2dd62beab77f2ba7c7a288ed8aa
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-04-25 19:04:06 +0200

    feat: added location display  Activity

---
+++ AB2/Code/app/src/main/java/com/example/assignment3/RotationIntents/DisplayerActivity.kt
@@ -0,0 +1,27 @@
+package com.example.assignment3.RotationIntents
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.assignment3.R
+
+class DisplayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_displayer)
+
+        val txtLocation = findViewById<TextView>(R.id.txtLocation)
+        txtLocation.text = intent.getStringExtra("location")
+
+        val btnLocate = findViewById<Button>(R.id.btnLocate)
+        btnLocate.setOnClickListener{
+            val url = "geo:?q=${txtLocation.text}"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+        }
+
+    }
+}
